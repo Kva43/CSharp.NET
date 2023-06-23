bdc3496d96d31fe7038709f2ef663f835da0e20b
Cristian Cabanillas <cristiancabanillas2001@gmail.com>
2023-06-22 23:15:11 -0300

    Subo algunos ejercicios

---
+++ 02-ClasesYMetodosEstaticos_Ejercicio9/Ejercicio 3 CLASE 02/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número para calcular su factorial: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int resultado = CalcularFactorial(num);
+
+        Console.WriteLine($"El factorial de {num} es {resultado}");
+        Console.ReadKey();
+    }
+
+    static int CalcularFactorial(int num)
+    {
+        int factorial = 1;
+
+        for (int i = num; i > 0; i--)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
---
+++ 02-ClasesYMetodosEstaticos_Ejericicio3/02-ClasesYMetodosEstaticos_Ejericicio3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Biblioteca;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int decimalNumber = 25;
+        string binaryNumber = Conversor.ConvertirDecimalABinario(decimalNumber);
+        Console.WriteLine($"El número binario equivalente a {decimalNumber} es: {binaryNumber}");
+
+        int binarioNumero = 11001;
+        int decimalNumero = Conversor.ConvertirBinarioADecimal(binarioNumero);
+        Console.WriteLine($"El número decimal equivalente a {binarioNumero} es: {decimalNumero}");
+
+    }
+}
